6083df18c5ac77b1dd314b77f93f3a1240f9709f
nelsci <nelson.lombardo@gmail.com>
2021-03-05 00:35:58 -0300

    wip

--- ass/fourier_phasors/scripts/sketch.js
+++ ass/fourier_phasors/scripts/sketch.js
@@ -39,7 +39,7 @@ function setup() {
   gterms.position(15, h);
   slider_terms = createSlider(1, 20, kv.get('terms'));
   slider_terms.parent(gterms);
-  label_terms = createSpan(' Segmentos');
+  label_terms = createSpan(' - Segmentos +');
   label_terms.style('color', '#FFFFFF');
   label_terms.parent(gterms);
   h += kh;
@@ -48,7 +48,7 @@ function setup() {
   gtime.position(15, h);
   slider_time = createSlider(1, 100, kv.get('time'));
   slider_time.parent(gtime);
-  label_time = createSpan(' Velocidad');
+  label_time = createSpan(' - Velocidad +');
   label_time.style('color', '#FFFFFF');
   label_time.parent(gtime);
   h += kh;
@@ -57,7 +57,7 @@ function setup() {
   gamp.position(15, h);
   slider_amp = createSlider(10, canvas_h / 4, kv.get('amp'));
   slider_amp.parent(gamp);
-  label_amp = createSpan(' Tamaño');
+  label_amp = createSpan(' - Tamaño +');
   label_amp.style('color', '#FFFFFF');
   label_amp.parent(gamp);
   h += kh;
@@ -69,6 +69,18 @@ function setup() {
   label_func = createSpan(' Función');
   label_func.style('color', '#FFFFFF');
   label_func.parent(gfunc);
+  h += kh;
+
+  gorb = createDiv('');
+  gorb.position(15, h);
+  selorb = createSelect();
+  selorb.parent(gorb);
+  label_orb = createSpan(' Órbitas');
+  label_orb.style('color', '#FFFFFF');
+  label_orb.parent(gorb);
+  selorb.option('Con');
+  selorb.option('Sin');
+  selorb.selected(kv.get('orb'));
 
   fncs.forEach(function(item, idx) {
     sel.option(item.name);
@@ -102,17 +114,38 @@ window.addEventListener("beforeunload", function (e) {
   kv.set('terms', slider_terms.value());
   kv.set('data', data);
   kv.set('wave', wave);
+  kv.set('orb', selorb.value());
   return confirmationMessage;                            //Webkit, Safari, Chrome
 });
 
 function draw() {
   background(0);
-  translate(canvas_w / 4, canvas_h / 2);
   let radius = slider_amp.value();
   let i;
+  let kr = 1.2;
   data = old.data;
   wave = old.wave;
   old.data = { x: 0, y: 0, j: 0, n: 0, r: 0};
+
+  // Axis
+  translate(0, 0);
+  stroke('rgb(255, 255, 255)');
+  textSize(18);
+
+  text('Im(Z)', canvas_w / 4 + 10, 18);
+  line(canvas_w / 4, 0, canvas_w / 4, canvas_h);
+
+  text('Re(Z)', canvas_w / 2 - 100, canvas_h / 2 - 10);
+  line(0, canvas_h / 2, canvas_w / 2 - 20, canvas_h / 2);
+
+  text('f(t)', canvas_w / 2 + 10, 18);
+  line(canvas_w / 2, 0, canvas_w / 2, canvas_h);
+
+  text('t', canvas_w - 20, canvas_h / 2 - 10);
+  line(canvas_w / 2, canvas_h / 2, canvas_w, canvas_h / 2);
+
+  // Reset start point
+  translate(canvas_w / 4, canvas_h / 2);
 
   for (i = data.j; i < slider_terms.value(); i++) {
     let prevx = data.x;
@@ -130,7 +163,8 @@ function draw() {
 
     stroke('rgb(255, 0, 0)');
     noFill();
-    ellipse(prevx, prevy, data.r * 2);
+    if (selorb.value() == 'Con')
+      ellipse(prevx, prevy, data.r * 2);
 
     stroke('rgb(0, 255,0)');
     line(prevx, prevy, data.x, data.y);
@@ -138,9 +172,9 @@ function draw() {
   }
   wave.unshift(data.y);
 
-  translate((canvas_h / 2) + data.r * 1.2, 0);
+  translate((canvas_h / 2) + data.r * kr, 0);
   stroke('rgb(0, 0, 255)');
-  line(data.x - (canvas_h / 2) - data.r * 1.2, data.y, 0, wave[0]);
+  line(data.x - (canvas_h / 2) - data.r * kr, data.y, 0, wave[0]);
   beginShape();
   noFill();
   stroke('rgb(0, 255, 0)');
