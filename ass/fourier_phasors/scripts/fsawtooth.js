9d3d40e4f2fcd7458e2853738576a21a12d210bf
nelsci <nelson.lombardo@gmail.com>
2021-03-05 15:43:05 -0300

    Minor fixed

--- ass/fourier_phasors/scripts/fsawtooth.js
+++ ass/fourier_phasors/scripts/fsawtooth.js
@@ -1,6 +1,6 @@
 function fsawtooth(data, radius) {
   data.n = data.j + 1;
-  data.r = radius * (-4 / (data.n * Math.PI)) * Math.pow(-1, data.n);
+  data.r = radius * (-1 / (data.n * Math.PI));
   data.x += data.r * cos(data.n * time * 2 * Math.PI);
   data.y += data.r * sin(data.n * time * 2 * Math.PI);
 }
